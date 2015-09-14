2671643e79ddca9313b8fc13120acd0e5091c50b
Shaojun <968538@163.com>
2015-09-15 02:23:11 +0800

    基础环境

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//default controller
+class Welcome extends CI_Controller{
+	
+	public function index() {
+		echo 'hello';
+	}
+}
