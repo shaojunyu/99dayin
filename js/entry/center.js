bc0fb589ac8ee6ec4a0daa8a435cf03bb1dd59e5
jimmy <jimmyVV@qq.com>
2015-10-23 07:28:59 +0800

    123

--- js/entry/center.js
+++ js/entry/center.js
@@ -132,12 +132,12 @@ require(['jquery', 'scroll', 'utility', 'prompt', 'enroll', 'modal', 'ping'], fu
                         })
                         .done(function(data) {
                                 //如果发送支付请求成功，弹出模态框，然后再另外定位一个网页
-                                  window.open('./user/pay?orderId='+data.id);
-                                  console.log(data);
+                                $('#form').attr('action',$('#form').attr('action')+"?orderId="+data.id)
+                                .submit();
+                                window.open('./user/pay?orderId='+data.id);                                  
                                 openModal(_this.checkout_modal, false);
                                 _this.checkout_modal.attr('data-num', num); //修改模态框的订单号
-                              
-                                
+                                                                
                         })
                 }
             });
