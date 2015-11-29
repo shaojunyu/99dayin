c8df0c012e1cf0b412715deb246a922bdece92d3
yusj <968538@163.com>
2015-11-29 15:31:20 +0800

    oss

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,6 +1,7 @@
 <?php
 use Qiniu\Auth;
 use Qiniu\Storage\BucketManager;
+use Sts\Request\V20150401 as Sts;
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test extends CI_Controller{
@@ -15,14 +16,47 @@ class Test extends CI_Controller{
 		$qiniu_accessKey = 'kbnA7-cyf2y4j3JmmB8xKcQszBtQvpl45TAFMZ2z';
 		$qiniu_secretKey = 'e4gSw3iZxrOGI372CjaeMwP6Rif_2ekqfEbPgybA';
 		$this->qiniu_auth = new Auth($qiniu_accessKey, $qiniu_secretKey);
+		
+		//aliyun oss
+		require APPPATH.'third_party/aliyun/aliyun-php-sdk-core/Config.php';
+		// 你需要操作的资源所在的region，STS服务目前只有杭州节点可以签发Token，签发出的Token在所有Region都可用
+		// 只允许子用户使用角色
+		$iClientProfile = DefaultProfile::getProfile("cn-hangzhou", "GtzMAvDTnxg72R04", "VhD2czcwLVAaE7DReDG4uEVSgtaSYK");
+		$client = new DefaultAcsClient($iClientProfile);
+		
+		// 角色资源描述符，在RAM的控制台的资源详情页上可以获取
+		$roleArn = "<role-arn>";
+		// 在扮演角色(AssumeRole)时，可以附加一个授权策略，进一步限制角色的权限；
+		// 详情请参考《RAM使用指南》
+		// 此授权策略表示读取所有OSS的只读权限
+		$policy=<<<POLICY
+{
+  "Statement": [
+    {
+      "Action": [
+        "oss:Get*",
+        "oss:List*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ],
+  "Version": "1"
+}
+POLICY;
+		
+		$request = new Sts\AssumeRoleRequest();
+		// RoleSessionName即临时身份的会话名称，用于区分不同的临时身份
+		// 您可以使用您的客户的ID作为会话名称
+		$request->setRoleSessionName("99dayin");
+		$request->setRoleArn($roleArn);
+		$request->setPolicy($policy);
+		$request->setDurationSeconds(3600);
+		$response = $client->doAction($request);
+		print_r("\r\n");
+		print_r($response);
 	}
 	
 	public function index(){
-		$cart = new MY_Cart();
-		//($cart->addItem('123', '4567'));
-		//$cart->deleteItem('Fteg9ocVkLv0NHFTRd0kB3KUqxG-');
-		//var_dump($cart->getItems());
-		$order = new MY_Order();
-		$order->createPingPay();
 	}
 }
