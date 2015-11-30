1c6dd568b27382f92896e2f54bcaf2ff82f0f94a
yusj <968538@163.com>
2015-11-30 22:16:44 +0800

    后台迁移至bmob

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -2,6 +2,7 @@
 use Qiniu\Auth;
 use Qiniu\Storage\BucketManager;
 use Sts\Request\V20150401 as Sts;
+use OSS\OssClient;
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test extends CI_Controller{
@@ -11,66 +12,50 @@ class Test extends CI_Controller{
 		parent::__construct();
 		require_once APPPATH.'third_party/leancloud/AV.php';
 		
-		//引入qiniu
-		require_once APPPATH.'third_party/qiniu/autoload.php';
-		$qiniu_accessKey = 'kbnA7-cyf2y4j3JmmB8xKcQszBtQvpl45TAFMZ2z';
-		$qiniu_secretKey = 'e4gSw3iZxrOGI372CjaeMwP6Rif_2ekqfEbPgybA';
-		$this->qiniu_auth = new Auth($qiniu_accessKey, $qiniu_secretKey);
 
 	}
 	
 	public function index(){
+		require_once APPPATH.'third_party/aliyun_oss/vendor/autoload.php';
+		$accessKeyId = "GtzMAvDTnxg72R04"; ;
+		$accessKeySecret = "VhD2czcwLVAaE7DReDG4uEVSgtaSYK";
+		$endpoint = "oss-cn-hangzhou.aliyuncs.com";
+		try {
+			$oss_client = new OssClient($accessKeyId, $accessKeySecret, $endpoint);
+		} catch (Exception $e) {
+			var_dump($e);
+		}
+		$oss_client->setTimeout(3600);
+		$oss_client->setConnectTimeout(30);
+		$object = "user_upload/test.ppt";
+		try {
+			//$exist = $oss_client->doesObjectExist('dayin', $object);
+			$objectMeta = $oss_client->getObjectMeta('dayin', $object);
+			//var_dump($objectMeta);
+		} catch (Exception $e) {
+			var_dump($e->getMessage());
+		}
+		
+		
+		
+		//下载文件
+		$localfile = "./temp/test.ppt";
+		$options = array(
+				OssClient::OSS_FILE_DOWNLOAD => $localfile,
+		);
+		try {
+			//$oss_client->getObject('dayin', $object, $options);
+		} catch (Exception $e) {
+			var_dump($e);
+		}
+		
+		try {
+			$command = APPPATH.'third_party/exe/PPTPages'.' '.$localfile;
+			exec($command,$output);
+			var_dump($output);
+		} catch (Exception $e) {
+		}
+		
+		
 	}
-	
-	function gmt_iso8601($time) {
-		$dtStr = date("c", $time);
-		$mydatetime = new DateTime($dtStr);
-		$expiration = $mydatetime->format(DateTime::ISO8601);
-		$pos = strpos($expiration, '+');
-		$expiration = substr($expiration, 0, $pos);
-		return $expiration."Z";
-	}
-	
-	function get_token(){
-		require_once APPPATH.'third_party/oss_php_sdk_20140625/sdk.class.php';
-		$id= 'GtzMAvDTnxg72R04';
-		$key= 'VhD2czcwLVAaE7DReDG4uEVSgtaSYK';
-		$host = 'http://dayin.oss-cn-hangzhou.aliyuncs.com';
-		$now = time();
-		$expire = 30; //设置该policy超时时间是10s. 即这个policy过了这个有效时间，将不能访问
-		$end = $now + $expire;
-		$expiration = $this->gmt_iso8601($end);
-		
-		$oss_sdk_service = new alioss($id, $key, $host);
-		$dir = 'user-dir/';
-		
-		//最大文件大小.用户可以自己设置
-		$condition = array(0=>'content-length-range', 1=>0, 2=>1048576000);
-		$conditions[] = $condition;
-		
-		//表示用户上传的数据,必须是以$dir开始, 不然上传会失败,这一步不是必须项,只是为了安全起见,防止用户通过policy上传到别人的目录
-		$start = array(0=>'starts-with', 1=>'$key', 2=>$dir);
-		$conditions[] = $start;
-		
-		
-		//这里默认设置是２０２０年.注意了,可以根据自己的逻辑,设定expire 时间.达到让前端定时到后面取signature的逻辑
-		$arr = array('expiration'=>$expiration,'conditions'=>$conditions);
-		//echo json_encode($arr);
-		//return;
-		$policy = json_encode($arr);
-		$base64_policy = base64_encode($policy);
-		$string_to_sign = $base64_policy;
-		$signature = base64_encode(hash_hmac('sha1', $string_to_sign, $key, true));
-		
-		$response = array();
-		$response['accessid'] = $id;
-		$response['host'] = $host;
-		$response['policy'] = $base64_policy;
-		$response['signature'] = $signature;
-		$response['expire'] = $end;
-		//这个参数是设置用户上传指定的前缀
-		$response['dir'] = $dir;
-		echo json_encode($response);
-	}
-	
 }
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,37 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-use leancloud\AVUser;
-use Qiniu\Auth;
-use Qiniu\Storage\BucketManager;
-
 
 //default controller
 class Welcome extends CI_Controller{
 	
-	var $qiniu_auth;
 	
 	public function __construct(){
 		parent::__construct();
-		//引入leancloud
-		require_once APPPATH.'/third_party/leancloud/AV.php';
-		
-		//引入qiniu
-		require_once APPPATH.'third_party/qiniu/autoload.php';
-		$qiniu_accessKey = 'kbnA7-cyf2y4j3JmmB8xKcQszBtQvpl45TAFMZ2z';
-		$qiniu_secretKey = 'e4gSw3iZxrOGI372CjaeMwP6Rif_2ekqfEbPgybA';
-		$this->qiniu_auth = new Auth($qiniu_accessKey, $qiniu_secretKey);
-		
-		//引入ping++
-		require_once APPPATH.'third_party/pingpp/init.php';
-		$pingpp_testKey = 'sk_test_0CKaPS8CmDeLfr9CCOmXHGGS';
-		$pingpp_liveKey = 'sk_live_bOz9YlaOHrS7dFw9yYlUif7R';
-		Pingpp\Pingpp::setApiKey($pingpp_testKey);
 	}
 	
 	public function index() {
 		$this->load->view('index_page');
-		//$bucket = new BucketManager($this->qiniu_auth);
-		set_cookie('token','123456');
 	}
 }
