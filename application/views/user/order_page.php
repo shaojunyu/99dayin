f65f274367681504f18db13a27a30018e22baf9d
yusj <968538@163.com>
2015-12-15 21:46:36 +0800

    api

--- application/views/user/order_page.php
+++ application/views/user/order_page.php
@@ -166,7 +166,7 @@
                                 <div class="order-btns">
                                 <form action="./order" method="get" id="form" target="_blank"></form>
                                     <a class="go-pay"  target="view_window" href="./pay?orderId=<?php echo $order->objectId;?>" data-seq='1'>去付款</a>
-                                    <a class="cancel" data-order="12324412312" href="javascript:void(0)">取消订单</a>
+                                    <a class="cancel" data-order="<?php echo $order->objectId;?>" href="javascript:void(0)">取消订单</a>
                                 </div>
                                 <div class="summary">
                                     <div class="summary-info">
