d585829b21b25d00050457481494c8e457e24947
yusj <968538@163.com>
2015-12-28 20:10:17 +0800

    page

--- application/views/public/page_header.php
+++ application/views/public/page_header.php
@@ -5,7 +5,7 @@
                 <span>99打印</span>
             </a>
             <div class="titles">
-                <a href="javascript:void(0)" class="title-btn homepage-btn" data-content="首页">首页</a>
+                <a href="http://www.99dayin.com" class="title-btn homepage-btn" data-content="首页">首页</a>
                 <a href="javascript:void(0)" class="title-btn introduction" data-content="简介">简介</a>
                 <!-- <div class="title-btn art-base" data-content="本校文库">本校文库</div> -->
                 <div class="title-btn">
@@ -17,8 +17,8 @@
                 </div>
                 </div>
                  <div class="detail-btn">
-                    <a href="./orders">个人中心</a>
-                    <a href="./upload">打印列表</a>
+                    <a href="./<?php echo strpos(current_url(),'user/')?'':'user/';?>orders">个人中心</a>
+                    <a href="./<?php echo strpos(current_url(),'user/')?'':'user/';?>upload">打印列表</a>
                     <a class="logout" href="javascript:void(0)">退出登录</a>
                 </div>
          </div>
