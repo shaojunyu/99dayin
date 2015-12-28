2c231c519bce508d578f5c1062e3950afc8f361f
jimmy <jimmyVV@qq.com>
2015-12-28 21:33:23 +0800

    new

--- application/views/index_page.php
+++ application/views/index_page.php
@@ -71,12 +71,7 @@
         <div>
             <button class="confirm-btn login-btn" id="confirm-btn">确认</button>
         </div>
-<<<<<<< HEAD
-       <!--  <div class="link-login">
-            其他方式登录:<span class="QQ" id="QQ-login"><i class="logo-QQ"></i>QQ登录</span><span class="wei"><i class="logo-wei"></i>微信登录</span>
-        </div> -->
-=======
->>>>>>> f0ca365a14eab76d43777502c929a154c7cf1e6f
+
         <div class="login-choice">
             <div class="user-login active">
                 <div>用</div>
@@ -84,16 +79,7 @@
                 <div>登</div>
                 <div>录</div>
             </div>
-<<<<<<< HEAD
-           <!--  <div class="store-login ">                
-                <div>打</div>
-                <div>印</div>
-                <div>店</div>
-                <div>登</div>
-                <div>录</div>
-            </div> -->
-=======
->>>>>>> f0ca365a14eab76d43777502c929a154c7cf1e6f
+
         </div>
     </div>
    <div class="signin-frame" id="signin-frame">
