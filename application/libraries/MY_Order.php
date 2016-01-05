c6a791ab20e099f1ae9cbfd89ae99510a773d534
yusj <968538@163.com>
2016-01-05 15:21:12 +0800

    t

--- application/libraries/MY_Order.php
+++ application/libraries/MY_Order.php
@@ -168,8 +168,8 @@ class MY_Order extends MY_Base_Class{
 			$ch = Pingpp\Charge::create(array(
 					'subject' => '99打印在线支付',
 					'body' => '文档打印',
-					'amount'=>10,
-					'order_no'=>rand(1,99999),
+					'amount'=>$res->totalPrice,
+					'order_no'=>$orderId,
 					'currency'  => 'cny',
 					'extra'     => $extra,
 					'channel'   => $channel,
@@ -179,24 +179,25 @@ class MY_Order extends MY_Base_Class{
 	
 			//更新订单的信息
 			//var_dump($ch->__toStdObject());
-			//$this->bmobOrder->update($orderId,array('pingppId'=>$ch->__toArray()['id']));
+			$this->bmobOrder->update($orderId,array('pingppId'=>$ch->__toArray()['id']));
 			$alipay_pc_direct = $ch->__toStdObject()->credential->alipay_pc_direct;
-			var_dump($ch->__toStdObject()->credential->alipay_pc_direct);
+			//var_dump($ch->__toStdObject()->credential->alipay_pc_direct);
 			$alipay_pc_direct_array = array();
 			foreach ($alipay_pc_direct as $k => $v){
 				$alipay_pc_direct_array[$k] = $v;
 			}
-			var_dump($alipay_pc_direct_array);
-			$this->argSort($alipay_pc_direct_array);
+			//var_dump($alipay_pc_direct_array);
+			//$this->argSort($alipay_pc_direct_array);
+			echo '正在跳转至支付宝支付页面...';
 			$sHtml = "<form id='alipaysubmit' name='alipaysubmit' action='https://mapi.alipay.com/gateway.do?_input_charset=utf-8' method='post'>";
 			while (list ($key, $val) = each ($alipay_pc_direct_array)) {
-				$sHtml.= $key.":<input type='' name='".$key."' value='".$val."'/><br/>";
+				$sHtml.= "<input type='hidden' name='".$key."' value='".$val."'/><br/>";
 			}
 				
 			//submit按钮控件请不要含有name属性
-			$sHtml = $sHtml."<input type='submit' value='提交'></form>";
+			//$sHtml = $sHtml."<input type='submit' value='提交'></form>";
 				
-			$sHtml = $sHtml."<script>document.forms['alipaysubmit'];</script>";
+			$sHtml = $sHtml."<script>document.forms['alipaysubmit'].submit();</script>";
 			echo  $sHtml;
 		} catch (\Pingpp\Error\Base $e) {
 			header('Status: ' . $e->getHttpStatus());
